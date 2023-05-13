1d350fca62c32252eed309cabaea42982e205ce6
monkeytitle <monkeytitle@hotmail.com>
2023-05-13 14:29:50 +0700

    Connecting to database

--- Bank.java
+++ Bank.java
@@ -4,11 +4,51 @@
  */
 package bankapplication03;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 /**
  *
  * @author monke
  */
 public class Bank {
-  
+  String name;
+  Connection connect;
+  void openAccount(Account account){
    
 }
+  void closeAccount(int number){
+      
+  }
+  void depositMoney(Account account,double amount){
+      
+  }
+  void withdrawMoney(Account account,double amount){
+      
+  }
+  void listAccounts() throws SQLException{
+      String sql = "SELECT * FROM ";
+      connect = BankConnection.connect();
+      Statement statement = connect.createStatement();
+      ResultSet result = statement.executeQuery(sql);
+      while(result.next()){
+          System.out.println(result.getString("accountID"));
+      }
+      
+  }
+    public static void main(String[] args) throws SQLException {
+        String sql = "SELECT * FROM accounts";
+      var connect = BankConnection.connect();
+      Statement statement = connect.createStatement();
+      ResultSet result = statement.executeQuery(sql);
+      while(result.next()){
+          System.out.print(result.getString("balance"));
+        
+      }
+    }
+}
+//  Account getAccount(int number )
+//          
+//}
--- BankApplication03.java
+++ BankApplication03.java
@@ -5,6 +5,8 @@
 package bankapplication03;
 
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,7 +17,7 @@ public class BankApplication03 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
         int option = 0;
         
@@ -23,12 +25,35 @@ public class BankApplication03 {
             Scanner scan = new Scanner(System.in);
             System.out.println("1.Display All Accounts");
             System.out.println("2.Open new Account");
-            System.out.println("3.Cloase Existing Account");
+            System.out.println("3.Close Existing Account");
             System.out.println("4.Desposit:");
             System.out.println("5.Withdraw");
             System.out.println("Enter option:");
             option = scan.nextInt();
-            System.out.println(option);
+//            System.out.println(option);
+switch(option){
+    case 1:
+        
+        
+        break;
+    case 2:
+    break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+         break;
+         
+    case 6:
+        break;
+        
+           
+    
+    
+     
+        
+}
         }
     }
     
